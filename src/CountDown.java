16f8678efbb0f6ad839c7b43df57e74ec713c07c
Sym Conger <acesilverryuu@gmail.com>
2019-05-09 13:23:53 -0500

    thing

--- src/CountDown.java
+++ src/CountDown.java
@@ -1,21 +1,48 @@
 public class CountDown {
     public static void main (String[] args)
     {
-        System.out.println("FINAL COUNTDOWN!!!!!");
-
+//        System.out.println("FINAL COUNTDOWN!!!!!");
+//
+//        int count = 10;
+//
+//        while (count != 0)
+//        {
+//            System.out.println(count);
+//            --count;
+//        }
+//int count2 = 0;
+//        while (count2 <= 10)
+//        {
+//            System.out.println(count2);
+//            count2++;
+//        }
+// displayDoWhile();
+        displayForLoop();
+    }
+    public static void displayDoWhile ()
+    {
         int count = 10;
-
-        while (count != 0)
-        {
+        int count2 = 10;
+        do {
             System.out.println(count);
             --count;
         }
-
-        while (count != 11)
+        while (count>=0);
+        do {
+            System.out.println(count2);
+            ++count2;
+        } while (count2<=10);
+    }
+    public static void displayForLoop()
+    {
+        for (int j = 10; j>= 1; j--)
         {
-            System.out.println(count);
-            count++;
+            System.out.println(j);
         }
 
+        for(int i = 0; i<=10; i++)
+        {
+            System.out.println(i);
+        }
     }
 }
