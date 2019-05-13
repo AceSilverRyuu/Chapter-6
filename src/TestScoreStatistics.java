0f58f4b651bcdfe809d29dbf2a8d3aae4751e755
Sym Conger <acesilverryuu@gmail.com>
2019-05-13 12:46:29 -0500

    test score update

--- src/TestScoreStatistics.java
+++ src/TestScoreStatistics.java
@@ -2,46 +2,50 @@ import java.util.Scanner;
 
 public class TestScoreStatistics {
 
-    public static void main (String[]args){
+    public static void main (String[]args) {
 
         Scanner input = new Scanner(System.in);
 
-        int userChoice = 0;;
+        int userChoice = 0;
+        final int MAX = 100;
+        final int MIN = 0;
         int count = 0;
-        int high = 0;
-        int low = 100;
+        int high;
+        int low;
         int totalScore = 0;
-        double average = 0;
+        double average;
 
 
+        System.out.println("Please enter a number between 0 & 100 or type 999 to quit >>>> ");
+        userChoice = input.nextInt();
+        
+        high = userChoice;
+        low = userChoice;
+        while (userChoice != 999) {
 
+            if (userChoice <= MAX && userChoice >= MIN) {
+                ++count;
+                totalScore = totalScore + userChoice;
 
-        while (userChoice != 999){
+                if (userChoice > high)
 
-            System.out.println("Please enter a number between 0 & 100 or type 999 to quit >>>> ");
-            userChoice = input.nextInt ();
+                    high = userChoice;
+                    if (userChoice < low)
 
-            if (userChoice > high){
-
-                high = userChoice;
+                        low = userChoice;
+                } else
+                    System.out.println("Type another score or type 999 to exit.");
+                userChoice = input.nextInt();
             }
-            else if (userChoice < low){
 
-                low = userChoice;
-            }
-           count++;
-        }
-        if (userChoice == 999) {
 
-            totalScore = totalScore + userChoice;
+            totalScore = totalScore + userChoice - 999;
             average = totalScore / count;
 
             System.out.println ("You entered " + count + " test scores");
             System.out.println ("The Highest score was " + high);
             System.out.println ("The Lowest score was " + low);
             System.out.println ("The Average was " + average);
-
-        }
 
 
     }
